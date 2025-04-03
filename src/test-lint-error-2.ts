2b8c4f92fef736ad8962038155893d6eb92ed4a9
Bruno Camarneiro <camarneirobruno@gmail.com>
2025-04-03 14:37:00 +0200

    Add pre-commit hook with Biome linting

--- src/test-lint-error-2.ts
+++
@@ -1,8 +0,0 @@
-// This file contains a linting error that should block the commit
-function anotherBadFunction() {
-  // Unused variable should be caught by the linter
-  const unusedVariable = 'This will cause a linting error';
-  return 'test';
-}
-
-export default anotherBadFunction;
